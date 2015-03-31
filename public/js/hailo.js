b6e59f56cec93c341da711c11cfbce363abfff45
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-31 18:00:48 +0100

    added search box functionality

--- public/js/hailo.js
+++ public/js/hailo.js
@@ -8,6 +8,45 @@ app.controller('mapController', function($scope, $http, uiGmapGoogleMapApi) {
 
   $scope.greeting= "hello";
 
+
+$scope.marker = {
+        coords: {
+            latitude: 51.5085300, longitude: -0.1257400
+        },
+        icon: 'https://www.hailoapp.com/assets/img/barty.svg',
+        options: { draggable: true }
+    }
+
+
+var events = {
+    places_changed: function (searchBox) {
+        var place = searchBox.getPlaces();
+        if (!place || place == 'undefined' || place.length == 0) {
+            console.log('no place data :(');
+            return;
+        }
+
+        $scope.map = {
+            "center": {
+                "latitude": place[0].geometry.location.lat(),
+                "longitude": place[0].geometry.location.lng()
+            },
+            "zoom": 14
+        };
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: place[0].geometry.location.lat(),
+                longitude: place[0].geometry.location.lng()
+            },
+            icon: 'https://www.hailoapp.com/assets/img/barty.svg'
+        };
+    }
+};
+$scope.searchbox = { template: 'searchbox.tpl.html', events: events };
+    // $scope.searchBox = { template:'searchBox.template.html', events:searchBoxEvents, parentdiv: 'searchBoxParent'};
+
+
         $scope.drivers = [];
 
         //  $http.get('https://api.hailoapp.com/drivers/near?latitude=51.5085300&longitude=-0.1257400&api_token=zr47c1qxafu1syNfns8KEmLLtcT9FE5Q9IGS4p6OI1ctyEjQP4mJpnmdiZZMH1YrxgyYm/09rOI2cXIrxdOBkVkxaPCN95OsDMpeENZ3dYEgaQgWAbDKDajr4V5CC2sUAucDrUtNPARMmGv2Cc7d9aDBftGJlSh8enCrIBI/VtC5LhsYFxJXBHr84dPCgV9B4fSwNlLMJYMFsOlSiwDjcA==')
@@ -43,12 +82,4 @@ $scope.map = {center: {latitude:  51.5085300, longitude: -0.1257400 }, zoom: 16,
 
     }
 
-$scope.marker = {
-        coords: {
-            latitude: 51.5085300, longitude: -0.1257400
-        },
-        icon: 'https://www.hailoapp.com/assets/img/barty.svg',
-        options: { draggable: true }
-    }
-
-      });
+});
