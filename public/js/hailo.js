e2fc56c1009dcc62fd5fcd5e3e895992eda889a1
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-31 16:36:25 +0100

    user can click and update location

--- public/js/hailo.js
+++ public/js/hailo.js
@@ -19,13 +19,14 @@ app.controller('mapController', function($scope, $http, uiGmapGoogleMapApi) {
         // });
 
 
-$scope.map = {center: {latitude:  51.5085300, longitude: -0.1257400 }, zoom: 14, events: {
+$scope.map = {center: {latitude:  51.5085300, longitude: -0.1257400 }, zoom: 16, events: {
         click: function (map, eventName, args) {
 
           $scope.drivers = [];
             $scope.marker.coords.latitude = args[0].latLng.lat();
             $scope.marker.coords.longitude = args[0].latLng.lng();
 
+             $scope.$apply();
 
              $http.get('https://api.hailoapp.com/drivers/near?latitude=' + $scope.marker.coords.latitude + '&longitude=' + $scope.marker.coords.longitude  + '&api_token=zr47c1qxafu1syNfns8KEmLLtcT9FE5Q9IGS4p6OI1ctyEjQP4mJpnmdiZZMH1YrxgyYm/09rOI2cXIrxdOBkVkxaPCN95OsDMpeENZ3dYEgaQgWAbDKDajr4V5CC2sUAucDrUtNPARMmGv2Cc7d9aDBftGJlSh8enCrIBI/VtC5LhsYFxJXBHr84dPCgV9B4fSwNlLMJYMFsOlSiwDjcA==')
           .success(function(data){
@@ -34,13 +35,12 @@ $scope.map = {center: {latitude:  51.5085300, longitude: -0.1257400 }, zoom: 14,
               $scope.drivers.push(data.drivers[i]);
             };
         });
-        },
-        zoom_changed: function(map, eventName, args) {
 
-          $scope.map.center = $scope.marker.coords
         }
 
+
     }
+
     }
 
 $scope.marker = {
