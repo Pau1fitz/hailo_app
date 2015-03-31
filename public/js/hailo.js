4557869a7eefbb0a35a9fddd3ac43b7bf71536a3
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-31 21:29:05 +0100

    abegan adding some style

--- public/js/hailo.js
+++ public/js/hailo.js
@@ -1,12 +1,37 @@
 'use strict';
 
-var app = angular.module('angularMapsTutorialApp',['uiGmapgoogle-maps']);
+// Defines that JavaScript code should be executed strict mode
 
+var app = angular.module('angularMapsTutorialApp',['uiGmapgoogle-maps']);
+//defines the Angular app to be used in view
 
 app.controller('mapController', function($scope, $http, uiGmapGoogleMapApi) {
 
+  var key = '&api_token=zr47c1qxafu1syNfns8KEmLLtcT9FE5Q9IGS4p6OI1ctyEjQP4mJpnmdiZZMH1YrxgyYm/09rOI2cXIrxdOBkVkxaPCN95OsDMpeENZ3dYEgaQgWAbDKDajr4V5CC2sUAucDrUtNPARMmGv2Cc7d9aDBftGJlSh8enCrIBI/VtC5LhsYFxJXBHr84dPCgV9B4fSwNlLMJYMFsOlSiwDjcA=='
 
+  //ui googlemap dependency injected here to enable maps
 
+    $scope.map =
+      {
+        center: {
+          latitude:  51.5085300,
+          longitude: -0.1257400
+        },
+        zoom: 15,
+        events: {
+          click: function (map, eventName, args) {
+            $scope.drivers = [];
+            $scope.marker.coords.latitude = args[0].latLng.lat();
+            $scope.marker.coords.longitude = args[0].latLng.lng();
+             $http.get('https://api.hailoapp.com/drivers/near?latitude=' + $scope.marker.coords.latitude + '&longitude=' + $scope.marker.coords.longitude  + key)
+            .success(function(data){
+              for (var i = 0; i < data.drivers.length; i++) {
+              $scope.drivers.push(data.drivers[i]);
+            };
+          });
+        }
+      }
+    }
 
 $scope.marker = {
         coords: {
@@ -27,17 +52,6 @@ var events = {
             return;
         }
 
-        $scope.drivers = [];
-         $scope.marker.coords.latitude = place[0].geometry.location.lat()
-        $scope.marker.coords.longitude = place[0].geometry.location.lng()
-            $http.get('https://api.hailoapp.com/drivers/near?latitude=' + $scope.marker.coords.latitude + '&longitude=' + $scope.marker.coords.longitude  + '&api_token=zr47c1qxafu1syNfns8KEmLLtcT9FE5Q9IGS4p6OI1ctyEjQP4mJpnmdiZZMH1YrxgyYm/09rOI2cXIrxdOBkVkxaPCN95OsDMpeENZ3dYEgaQgWAbDKDajr4V5CC2sUAucDrUtNPARMmGv2Cc7d9aDBftGJlSh8enCrIBI/VtC5LhsYFxJXBHr84dPCgV9B4fSwNlLMJYMFsOlSiwDjcA==')
-          .success(function(data){
-            for (var i = 0; i < data.drivers.length; i++) {
-
-              $scope.drivers.push(data.drivers[i]);
-            };
-        });
-
         $scope.map = {
 
             "center": {
@@ -46,6 +60,16 @@ var events = {
             },
             "zoom": 15
         };
+          $scope.drivers = [];
+        console.log($scope.marker.coords.latitude)
+         $scope.marker.coords.latitude = place[0].geometry.location.lat()
+        $scope.marker.coords.longitude = place[0].geometry.location.lng()
+            $http.get('https://api.hailoapp.com/drivers/near?latitude=' + $scope.marker.coords.latitude + '&longitude=' + $scope.marker.coords.longitude  + key)
+          .success(function(data){
+            for (var i = 0; i < data.drivers.length; i++) {
+              $scope.drivers.push(data.drivers[i]);
+            };
+        });
         $scope.marker = {
             coords: {
                 latitude: place[0].geometry.location.lat(),
@@ -56,32 +80,5 @@ var events = {
     }
 };
 
-$scope.searchbox = { template: 'searchbox.tpl.html', events: events };
-
-
-
-$scope.map = {center: {latitude:  51.5085300, longitude: -0.1257400 }, zoom: 16, events: {
-        click: function (map, eventName, args) {
-
-          $scope.drivers = [];
-            $scope.marker.coords.latitude = args[0].latLng.lat();
-            $scope.marker.coords.longitude = args[0].latLng.lng();
-
-             $scope.$apply();
-
-             $http.get('https://api.hailoapp.com/drivers/near?latitude=' + $scope.marker.coords.latitude + '&longitude=' + $scope.marker.coords.longitude  + '&api_token=zr47c1qxafu1syNfns8KEmLLtcT9FE5Q9IGS4p6OI1ctyEjQP4mJpnmdiZZMH1YrxgyYm/09rOI2cXIrxdOBkVkxaPCN95OsDMpeENZ3dYEgaQgWAbDKDajr4V5CC2sUAucDrUtNPARMmGv2Cc7d9aDBftGJlSh8enCrIBI/VtC5LhsYFxJXBHr84dPCgV9B4fSwNlLMJYMFsOlSiwDjcA==')
-          .success(function(data){
-            for (var i = 0; i < data.drivers.length; i++) {
-
-              $scope.drivers.push(data.drivers[i]);
-            };
-        });
-
-        }
-
-
-    }
-
-    }
-
+      $scope.searchbox = { template: 'searchbox.tpl.html', events: events };
 });
